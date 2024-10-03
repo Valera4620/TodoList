d5abd37386141e108953b0277209313ef024b336
Valera4620 <86981502+Valera4620@users.noreply.github.com>
2024-10-03 17:40:51 +0300

    ToDoListApp

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val userData: EditText = findViewById(R.id.user_data)
+        val button: Button = findViewById(R.id.button)
+
+        val toDos: MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,toDos)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener{adapterView,view,i,j ->
+            val text = listView.getItemAtPosition(i).toString()
+            adapter.remove(text)
+
+            Toast.makeText(this,"Deleted:$text",Toast.LENGTH_LONG).show()
+        }
+
+        button.setOnClickListener {
+            val text = userData.text.toString().trim()
+            if (text!= "")
+                adapter.insert(text,0)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
